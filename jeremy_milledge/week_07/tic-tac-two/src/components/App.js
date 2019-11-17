aa5d53313640c604898515d5297892d7173a2b65
Jeremy Milledge <jez.milledge@gmail.com>
2019-11-18 10:46:05 +1100

    bigO, recursion, tic-tac-two

---
+++ jeremy_milledge/week_07/tic-tac-two/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import Game from './Game.jsx'
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Game />
+    </div>
+  );
+}
+
+export default App;
