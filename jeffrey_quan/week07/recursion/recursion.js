c2eb1d574dda48f95a88aa76563a7df9957502e8
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-18 23:25:24 +1100

    including solutions from in class

--- jeffrey_quan/week07/recursion/recursion.js
+++ jeffrey_quan/week07/recursion/recursion.js
@@ -2,38 +2,60 @@
 // You will have to figure out what parameters to include
 // All functions must use recursion
 
-function findMax(array){
+function findMax(array, largest=-Infinity){
   // This function returns the largest number in a given array.
-  let max;
-  let n = array.length;
+  // My solution:
+  // let max;
+  // let n = array.length;
+  //
+  // function compare(a,b) {
+  //   if (a >= b) {
+  //     max = a;
+  //   } else {
+  //     max = b;
+  //   }
+  //   return max;
+  // }
+  //
+  // for (let i = 0; i < n; i++) {
+  //   if (i === 0) {
+  //     max = array[i];
+  //   } else if (i < n) {
+  //     compare(max, array[i]);
+  //   }
+  // }
+  //
+  // return max;
 
-  function compare(a,b) {
-    if (a >= b) {
-      max = a;
-    } else {
-      max = b;
-    }
-    return max;
+  // console.log('array', array, 'largest', largest);
+
+  if (array.length === 0){
+    return largest;
   }
 
-  for (let i = 0; i < n; i++) {
-    if (i === 0) {
-      max = array[i];
-    } else if (i < n) {
-      compare(max, array[i]);
-    }
+  if (array[0] > largest) {
+    largest = array[0];
   }
 
-  return max;
+  return findMax(array.slice(1), largest);
 }
 
 function factorial(n){
   // This function returns the factorial of a given number.
-  if (n === 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+  // My solution:
+  // if (n === 1) {
+  //   return 1;
+  // } else {
+  //   return n * factorial(n - 1);
+  // }
+
+  return (n <= 1) ? 1 : n * factorial(n - 1);
+
+  // if (n <= 1) {
+  //   return 1;
+  // } else {
+  //   return n * factorial(n - 1);
+  // }
 };
 
 function fibonacci(n){
@@ -49,26 +71,43 @@ function fibonacci(n){
   }
 }
 
-function coinFlips(n){
+function coinFlips(tosses){
   // This function returns an array of all possible outcomes from flipping a coin N times.
   // Input type: Integer
   // For example, coinFlips(2) would return the following:
   // ["HH", "HT", "TH", "TT"]
   // H stands for Heads and T stands for tails
   // Represent the two outcomes of each flip as "H" or "T"
-  if (n === 1) {
-    return ["H","T"];
-  } else {
-    let result = [];
-    arr1 = ["H","T"];
-    arr2 = coinFlips(n - 1);
-    for (let i = 0; i < arr1.length; i++) {
-      for (let j = 0; j < arr2.length; j++) {
-        result.push(arr1[i]+arr2[j]);
-      }
+
+  // My solution:
+  // if (n === 1) {
+  //   return ["H","T"];
+  // } else {
+  //   let result = [];
+  //   arr1 = ["H","T"];
+  //   arr2 = coinFlips(n - 1);
+  //   for (let i = 0; i < arr1.length; i++) {
+  //     for (let j = 0; j < arr2.length; j++) {
+  //       result.push(arr1[i]+arr2[j]);
+  //     }
+  //   }
+  //   return result;
+  // }
+
+  const combinations = [];
+
+  const flip = (soFar="") => {
+    if (soFar.length === tosses) {
+      combinations.push(soFar); // base case
+    } else {
+      flip(soFar + "H"); // recursive
+      flip(soFar + "T"); // recursive
     }
-    return result;
   }
+
+  flip();
+
+  return combinations;
 }
 
 function letterCombinations(arr){
@@ -76,25 +115,25 @@ function letterCombinations(arr){
   // Input type: Array of single characters
   // For example, letterCombinations(["a","b","c"]) would return the following:
   // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
-  if (arr.length === 1) {
-    return arr;
-  } else {
-    let result = arr;
-
-    for (let i = 0; i < arr.length; i++) {
-      const adder = arr[i];
-      const otherLetters = arr.filter( x => x !== adder);
-      console.log( otherLetters );
-      otherLetters.forEach(letter => { result.push( adder + letter ) });
-
-
-    }
-
-    return result;
-  }
+  // if (arr.length === 1) {
+  //   return arr;
+  // } else {
+  //   let result = arr;
+  //
+  //   for (let i = 0; i < arr.length; i++) {
+  //     const adder = arr[i];
+  //     const otherLetters = arr.filter( x => x !== adder);
+  //     console.log( otherLetters );
+  //     otherLetters.forEach(letter => { result.push( adder + letter ) });
+  //
+  //
+  //   }
+  //
+  //   return result;
+  // }
 }
 
-console.log( letterCombinations( ['a','b','c'] ));
+// console.log( letterCombinations( ['a','b','c'] ));
 
 module.exports = {
     findMax,
