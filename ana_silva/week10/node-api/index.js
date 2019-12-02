8d9dccdb9e9584032060ce36e1bccec983c94aed
Ana Silva <apgsilva81@gmail.com>
2019-12-03 10:29:08 +1100

    node-aoi

---
+++ ana_silva/week10/node-api/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const ejs = require('ejs');
+const axios = require('axios');
+const ejsLint = require('ejs-lint');
+
+
+const server = express();
+
+server.use(express.static('public'));
+server.use(express.urlencoded());
+server.use(express.json());
+
+server.set('view-engine', ejs);
+
+const PORT = 1337;
+
+server.get('/', (req,res) => {
+  res.render('home.ejs', "");
+
+
+});
+
+server.post('/', (req, res) => {
+  const query = req.body.query;
+  console.log(query);
+
+  //axios request
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`).then( (response) => {
+    console.log(response.data.items);
+    const results = response.data.items;
+    //get result into variable
+    // console.log(results);
+    // console.log(results[0].selfLink);
+    // console.log(results[0].volumeInfo.title);
+    // console.log(results[0].volumeInfo.imageLinks );
+
+    res.render('results.ejs', {results: results});
+
+  });
+
+
+
+})
+
+
+server.listen(PORT, () => console.log(`Now serving on http://localhost:${PORT}`));
